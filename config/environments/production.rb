c5ff0feec8173e771c5dfbda3e47ac11863f05a1
manju <manju@manjus-MacBook-Air.local>
2018-07-21 21:23:10 +0530

    Added mailer_option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,12 +83,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'https://damp-shore-51235.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'damp-shore-51235.herokuapp.com' }
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
    address: "smtp.gmail.com",
    port: 587,
-   domain: "google.com",
+   domain: "damp-shore-51235.herokuapp.com",
    authentication: "plain",
    enable_starttls_auto: true,
    user_name: "chauhanvanya24@gmail.com",
