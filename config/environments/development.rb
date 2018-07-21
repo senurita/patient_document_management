97d4c87a13912fe4a7ebbb335dbafb09707d5499
manju <manju@manjus-MacBook-Air.local>
2018-07-21 23:16:43 +0530

    Removed configs SMTP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,13 +56,5 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-   address: "smtp.gmail.com",
-   port: 587,
-   domain: "Pharmeasy_document_management",
-   authentication: "plain",
-   enable_starttls_auto: true,
-   user_name: "chauhanvanya24@gmail.com",
-   password: "vanya2324"
-}
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,17 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'damp-shore-51235.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'damp-shore-51235.herokuapp.com' , :protocol => 'http'}
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-   address: "smtp.gmail.com",
-   port: 587,
-   domain: "herokuapp.com",
-   authentication: "plain",
-   enable_starttls_auto: true,
-   user_name: "chauhanvanya24@gmail.com",
-   password: "vanya2324"
-}
+
 end
